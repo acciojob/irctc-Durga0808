fccd323d92ac836bbfeae6be4689739352fc6acb
Durga0808 <bejawada.durgaprasad@gmail.com>
2023-10-09 17:56:47 +0530

    completed

--- src/main/java/com/driver/services/TrainService.java
+++ src/main/java/com/driver/services/TrainService.java
@@ -112,7 +112,7 @@ public class TrainService {
         List<Ticket>tickets=train.getBookedTickets();
 
         for(Ticket t:tickets){
-            if(t.getFromStation()toString().equals(station.toString())){
+            if(t.getFromStation().toString().equals(station.toString())){
                 passengerCnt+=t.getPassengersList().size();
             }
         }
